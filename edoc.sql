3d5f6206cd50e56c477499e307b80d9be0bcf805
zhangjigang123 <57608706+zhangjigang123@users.noreply.github.com>
2019-11-14 11:37:09 +0800

    Add files via upload

---
+++ edoc.sql
@@ -0,0 +1,52 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.5.40 : Database - edoc
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`edoc` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `edoc`;
+
+/*Table structure for table `edoc_category` */
+
+DROP TABLE IF EXISTS `edoc_category`;
+
+CREATE TABLE `edoc_category` (
+  `id` int(10) NOT NULL,
+  `name` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `edoc_category` */
+
+/*Table structure for table `edoc_entry` */
+
+DROP TABLE IF EXISTS `edoc_entry`;
+
+CREATE TABLE `edoc_entry` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `categoryId` int(10) NOT NULL,
+  `title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `summary` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `uploaduser` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `createdate` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `categoryId` (`categoryId`),
+  CONSTRAINT `categoryId` FOREIGN KEY (`categoryId`) REFERENCES `edoc_entry` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `edoc_entry` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
